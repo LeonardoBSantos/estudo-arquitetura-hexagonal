c9cecebbd7183572c564b8460a649ad00eedd9ff
Leonardo santos <leonardosantos01@outlook.com>
2024-03-11 15:32:17 -0300

    caso 1 adaptadores api e console acessando a lógica de negócios do núcleo

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,5 @@
+using Application.services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,27 +10,12 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/sumInts", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var service = new Service();
+    var sum = service.SumInts();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return sum;
 });
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ Application/services/Service.cs
@@ -0,0 +1,30 @@
+﻿using Domain.entities;
+using Domain.entities.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.services
+{
+    public class Service : IService
+    {
+        public Entity1 entity1 { get; set; }
+        public Entity2 entity2 { get; set; }
+
+        public Service()
+        {
+             this.entity1 = new Entity1();
+             this.entity2 = new Entity2();
+        }
+
+        public int SumInts()
+        {
+            entity1.IntA = 2;
+            entity2.IntB = 2;
+
+            return entity1.IntA + entity2.IntB;
+        }
+    }
+}
